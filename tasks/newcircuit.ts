f4693311ad32823342f993469696514303d337f0
ASHISH1452 <108854792+ASHISH1452@users.noreply.github.com>
2023-08-02 10:46:53 -0700

    Update newcircuit.ts

--- tasks/newcircuit.ts
+++ tasks/newcircuit.ts
@@ -6,11 +6,21 @@ const fs = require("fs");
 const CONFIG_PATH = process.env.BASE_PATH + "/circuits.config.json";
 const DIR_PATH = process.env.BASE_PATH + "/circuits/";
 
+// Update the CircomCircuitConfig interface with additional parameters
+interface ExtendedCircomCircuitConfig extends CircomCircuitConfig {
+  author: string;
+  description: string;
+  // Add more parameters specific to your circuit
+  // e.g., constraintOrder: number, trustedSetupParticipants: string[], etc.
+}
+
 task("newcircuit", "Generate config for a new circuit")
   .addParam("name", "Name of the circuit")
+  .addParam("author", "Author of the circuit")
+  .addParam("description", "Description of the circuit")
   .setAction(async (taskArgs, {}) => {
 
-    let circuitsConfig: CircomCircuitConfig[] = [];
+    let circuitsConfig: ExtendedCircomCircuitConfig[] = [];
 
     // check if file exists
     if (fs.existsSync(CONFIG_PATH)) {
@@ -31,8 +41,8 @@ task("newcircuit", "Generate config for a new circuit")
     fs.closeSync(fs.openSync(DIR_PATH + taskArgs.name + "/input.json", 'w'))
     fs.closeSync(fs.openSync(DIR_PATH + taskArgs.name + "/circuit.circom", 'w'))
 
-    // create a new circuit config
-    const circuitConfig: CircomCircuitConfig = {
+    // create a new circuit config with additional parameters
+    const circuitConfig: ExtendedCircomCircuitConfig = {
       name: taskArgs.name,
       version: 2,
       protocol: "groth16",
@@ -42,8 +52,11 @@ task("newcircuit", "Generate config for a new circuit")
       zkey: taskArgs.name + "/out/" + taskArgs.name + ".zkey",
       vkey: taskArgs.name + "/out/" + taskArgs.name + ".vkey",
       r1cs: taskArgs.name + "/out/" + taskArgs.name + ".r1cs",
-      // Used when specifying `--deterministic` instead of the default of all 0s
       beacon: "0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f",
+      // Assign values to the additional parameters
+      author: taskArgs.author,
+      description: taskArgs.description,
+      // Add more parameter assignments here based on your requirements
     };
 
     // add circuit config to array
